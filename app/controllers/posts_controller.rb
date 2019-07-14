d5fd0ec9a7d79ef7c9cc70fe28e9bdb11e5bea3c
kakuko-deluxe <ky77ry@gmail.com>
2019-07-14 18:13:49 +0900

    Blog APP COMPLETE !!!

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
-    # @posts = Post.page(params[:page]).order(created_at: :desc).per(4)
     @new_posts = Post.all.find_newest_article
   end
   def new
